0b930bb9eebe21b1577f5aa0bb36174ef5b8d6d6
김용민 <dydals3440@gmail.com>
2023-03-26 20:42:31 +0900

    style: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <>
-      <header>
-        <Link to='/'>
-          <BsPrinterFill />
-          민화전사
+      <header className='flex justify-between border-b border-gray-300'>
+        <Link to='/' className='text-brand text-3xl flex m-4'>
+          <BsPrinterFill className='m-1' />
+          <h1>민화전사</h1>
         </Link>
-        <nav>
+        <nav className='flex items-center gap-4 font-semibold'>
           <Link to='/products'>Products</Link>
           <Link to='/carts'>Carts</Link>
-          <Link to='/products/new'>
+          <Link to='/products/new' className='text-2xl'>
             <BsPencilFill />
           </Link>
           <button>Login</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js, jsx}'],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#F96162',
+      },
+    },
   },
   plugins: [],
 };
